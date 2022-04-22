2cfb9f6048aeea26911271596b3fc76a4f5c934c
nikoladctrl <nikola.radanovic@digitalcontrol.me>
2022-04-22 13:19:47 +0200

    Config changes for deploy.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static('./dist/heroku'));
 
 app.get('/*', (req, res) => 
-    res.sendFile('index.html', { root: 'dist/kanban'})
+    res.sendFile('index.html', { root: 'dist/kanban-ng/'})
 );
 
 app.listen(process.env.PORT || 8080);
